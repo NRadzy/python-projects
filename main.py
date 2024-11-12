d63735f9046ef467ada9de3fd86cc0b4466706e5
Radhika <1radhika@gmail.com>
2024-11-12 10:41:44 -0800

    Added Env variables-using os and created Procfile

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
@@ -24,14 +25,11 @@ pip3 install -r requirements.txt
 
 This will install the packages from the requirements.txt for this project.
 '''
-
+# TODO: Configure Flask-Login
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
-
-
-# TODO: Configure Flask-Login
 
 
 # CREATE DATABASE
@@ -39,7 +37,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myBlogs.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', 'sqlite:///myBlogs.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -258,4 +256,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=False)
